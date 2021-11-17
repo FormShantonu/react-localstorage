29eda46325094de2d9e124be3e194f1650ca1cc8
Shantonu Chowdhury <chowdhury.shan93@gmail.com>
2021-11-17 11:05:43 +0530

    update local storage

--- localstorage/src/App.js
+++ localstorage/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Container } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
+import Todos from "./Component/Todos";
+import TodoFrom from "./Component/TodoFrom";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  // useEffect(()=>{},[]) syntext
+  // this function call automatically but if we want to add any dependency for that we have to 
+  useEffect(() => {
+    const localTodos = localStorage.getItem("todos");
+    console.log({ localStorage });
+    if (localTodos) {
+      setTodos(JSON.parse(localTodos));
+    }
+  }, []);
+
+  const addTodos = async todo=>{
+    setTodos([...todos, todo]);
+  }
+  // create the dependency
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos));
+  },[todos])
+
+  const markComplete = id=>{
+    setTodos(todos.filter(todo=>todo.id != id));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <h1>Todo with local storage</h1>
+      <Todos todos={todos} markComplete={markComplete} />
+      <TodoFrom addTodos={addTodos} />
+    </Container>
   );
-}
+};
 
 export default App;
